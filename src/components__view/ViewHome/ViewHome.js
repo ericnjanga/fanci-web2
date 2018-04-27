e0e4d6e0eb39e7d5da8c2a3687c5125bf6dbf424
Eric Njanga <njanga.webdev@gmail.com>
2018-04-27 16:48:15 -0400

    setup temporary map

--- src/components__view/ViewHome/ViewHome.js
+++ src/components__view/ViewHome/ViewHome.js
@@ -9,21 +9,20 @@ import img1 from './../../images/map.png';
 class ViewHome extends ViewApp {
   render () {
     return(
-      <Container className="view__content ViewHome">
-        <Row>
-          <Col>
-            <Figure img={img1} /> 
-            {/*<h2>Home view</h2> 
+      <div className="view__content ViewHome" style={{ backgroundImage:'url('+img1+')' }}> 
+        {/*<Row>
+          <Col><h2>Home view</h2> 
             
             <p>Biscuit pudding cupcake toffee dessert pastry. Muffin marshmallow chocolate bar. Dragée apple pie halvah pastry dessert jelly beans. Apple pie apple pie pie halvah cupcake marshmallow.</p>
             <p>Icing cake candy sugar plum pastry biscuit cake icing carrot cake. Croissant icing sesame snaps croissant candy marshmallow ice cream. Tiramisu cake toffee cheesecake jelly jelly-o sesame snaps pastry pie.</p>
             <p>Chupa chups bear claw tiramisu. Topping sweet topping muffin cupcake fruitcake danish. Caramels dessert toffee pudding marzipan. Pie danish tart cake.</p>
             <p>Biscuit pudding cupcake toffee dessert pastry. Muffin marshmallow chocolate bar. Dragée apple pie halvah pastry dessert jelly beans. Apple pie apple pie pie halvah cupcake marshmallow.</p>
             <p>Icing cake candy sugar plum pastry biscuit cake icing carrot cake. Croissant icing sesame snaps croissant candy marshmallow ice cream. Tiramisu cake toffee cheesecake jelly jelly-o sesame snaps pastry pie.</p>
-            <p>Chupa chups bear claw tiramisu. Topping sweet topping muffin cupcake fruitcake danish. Caramels dessert toffee pudding marzipan. Pie danish tart cake.</p>*/}
-          </Col>
+            <p>Chupa chups bear claw tiramisu. Topping sweet topping muffin cupcake fruitcake danish. Caramels dessert toffee pudding marzipan. Pie danish tart cake.</p>
+            </Col>
         </Row>
-      </Container> 
+        */}  
+      </div> 
     );
   }
 }//[end] Home
