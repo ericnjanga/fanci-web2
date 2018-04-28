7f191a813cb675c4c6a4abe3f7ec81ea31e48fa2
Eric Njanga <njanga.webdev@gmail.com>
2018-04-28 09:19:12 -0400

    styled small screen main nav

--- src/components__global/MenuPrimary.js
+++ src/components__global/MenuPrimary.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Nav, NavItem } from 'reactstrap';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'; 
+import faHome from '@fortawesome/fontawesome-free-solid/faHome';
+import faComment from '@fortawesome/fontawesome-free-solid/faComment';
+import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
 
 const MenuPrimary = (props) => { 
   return( 
     <Nav>
       { props.children }
-      <NavItem>
-        <NavLink to={`/`} exact={true}>Home</NavLink>
+      <NavItem> 
+        <NavLink to={`/`} exact>
+          <span className="sr-only">Home</span>
+          <small><FontAwesomeIcon icon={faHome} /></small>
+        </NavLink>
       </NavItem>
       <NavItem>
-        <NavLink to={`/about`}>About</NavLink>
-      </NavItem>
-      <NavItem>
-        <NavLink to={`/articles`}>Articles</NavLink>
-      </NavItem> 
-      <NavItem>
-        <NavLink to={`/gallery`}>Gallery</NavLink>
-      </NavItem> 
-      <NavItem>
-        <NavLink to={`/message-board`}>Message Board</NavLink>
+        <NavLink to={`/message-board`}>
+          <span className="sr-only">Around Us</span>
+          <small><FontAwesomeIcon icon={faComment} /></small>
+        </NavLink>
       </NavItem>  
+      <NavItem> 
+        <NavLink to={`/search`}>
+          <span className="sr-only">Around Us</span>
+          <small><FontAwesomeIcon icon={faSearch} /></small>
+        </NavLink>
+      </NavItem> 
     </Nav> 
   );
 }
